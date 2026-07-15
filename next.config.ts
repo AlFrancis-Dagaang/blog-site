d6ee3d3f7cb05f0b33012e65c4279022983d15bc
Al Francis H. Daga-ang <dagaang.alfrancis@gmail.com>
2026-07-15 11:54:50 +0800

    chore: configure Vercel Blob storage

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  images: {
+    remotePatterns: [
+      new URL("https://s43uw3zwlzrhjj9o.public.blob.vercel-storage.com/**"),
+    ],
+  },
 };
 
 export default nextConfig;
