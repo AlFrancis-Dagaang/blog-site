f0c9150fa1b774e2fcbb0d3ae78a238231f10a23
Al Francis H. Daga-ang <dagaang.alfrancis@gmail.com>
2026-07-15 19:54:36 +0800

    fix: update import paths to use node: prefix for crypto and fs modules

--- lib/auth/admin.ts
+++ lib/auth/admin.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { timingSafeEqual } from "crypto";
+import { timingSafeEqual } from "node:crypto";
 import { cookies } from "next/headers";
 
 const COOKIE_NAME = "admin_session";
